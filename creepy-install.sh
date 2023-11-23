8f5a0775edc1f1e359232d303d8e5aca840eac90
Tomasz Śmigielski <tomeksmigielski02@gmail.com>
2023-11-23 15:17:08 +0100

    Install script V1

---
+++ creepy-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ~
+ln -s .config/zshrc .zshrc
+mkdir .zsh
+cd .zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/lib/git.zsh > omz.lib.git.zsh
+wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh > omz.git.plugin.zsh
+
