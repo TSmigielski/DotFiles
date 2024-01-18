dc40e4a5bc41af23d70e3834a53af803d273336a
Tomasz <tomeksmigielski02@gmail.com>
2024-01-18 12:54:16 +0100

    More navigation

--- nvim/lua/tsmigiel/mappings.lua
+++ nvim/lua/tsmigiel/mappings.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<C-j>", "jzz")
+vim.keymap.set("n", "<C-k>", "kzz")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
