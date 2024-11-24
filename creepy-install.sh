1474d3237bb09d88b6a98194ef03c325bb55943b
Tomasz Śmigielski <tomeksmigielski02@gmail.com>
2024-11-24 18:35:44 +0100

    Autocomplete plugin

--- creepy-install.sh
+++ creepy-install.sh
@@ -7,5 +7,6 @@ mkdir .zsh
 cd .zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete
 wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/lib/git.zsh > omz.lib.git.zsh
 wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh > omz.git.plugin.zsh
