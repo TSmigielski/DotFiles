9e27d27b3d55a4ec1fefed8d6ee03245ca4bc7af
Tomasz Śmigielski <tomeksmigielski02@gmail.com>
2025-03-28 22:18:57 +0100

    Added rainbow brackets and indent lines

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -27,13 +27,6 @@ return {
   },
 
   {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = "BufReadPre",
-    opts = { }
-  },
-
-  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = { }
