032f548936e0dfd895586b0dec2ec0bdfe15fb42
Tomasz Śmigielski <tomeksmigielski02@gmail.com>
2025-04-11 14:52:24 +0200

    Changed vi mode plugin to manually managed

--- creepy-install.sh
+++ creepy-install.sh
@@ -8,5 +8,6 @@ cd .zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/marlonrichert/zsh-autocomplete
+git clone https://github.com/jeffreytse/zsh-vi-mode.git
 wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/lib/git.zsh > omz.lib.git.zsh
 wget -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh > omz.git.plugin.zsh
