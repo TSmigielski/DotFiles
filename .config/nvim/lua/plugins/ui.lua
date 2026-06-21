f58363c9565666e078d481896a766d6b76a463ef
Tomasz Śmigielski <tsmigiel@ik.me>
2026-06-21 13:02:57 +0200

    Changed snacks dashboard pickers to FZF

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -34,7 +34,20 @@ return {
       priority = 1000,
       lazy = false,
       opts = {
-         dashboard = { enabled = true },
+         dashboard = {
+            enabled = true,
+            preset = {
+               keys = {
+                  { icon = " ", key = "f", desc = "Find File", action = ":FzfLua files" },
+                  { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                  { icon = " ", key = "g", desc = "Find Text", action = ":FzfLua live_grep" },
+                  { icon = " ", key = "r", desc = "Recent Files", action = ":FzfLua oldfiles" },
+                  { icon = " ", key = "c", desc = "Config", action = ":FzfLua files cwd=~/.config/nvim" },
+                  { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                  { icon = " ", key = "q", desc = "Quit", action = ":qa" }
+               }
+            }
+         },
          input = { enabled = true },
          picker = { enabled = true },
          styles = {
